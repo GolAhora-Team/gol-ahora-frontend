69d817d92b202242f2ff098f5bd93b304336090f
Javier Florentin <javier.florentin.pri@gmail.com>
2026-05-29 19:06:07 -0300

    avisos para admin y personal

--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -183,7 +183,7 @@ export default function UserScreen({ route, navigation }) {
           await userService.createUsuarioAdmin(payload);
         }
 
-        Alert.alert("¡Usuario Creado!", `Credenciales generadas:\nUsuario/DNI: ${formData.dni}\nContraseña: 1234`);
+        alert(`¡Usuario Creado!\nCredenciales generadas:\nUsuario/DNI: ${formData.dni}\nContraseña: 1234`);
         loadUsers(); 
         setModalVisible(false);
       }
